d20368734d79860a47b1e83d34a5da4069e2e72c
distraction <yudubs47@gmail.com>
2022-07-04 18:33:30 +0800

    fix: 区分类型输出文件

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Main from './src/pages/main'
 import About from './src/pages/about'
 import './src/pages/other'
-
+import imgUrl from './src/assets/img/icon.png'
 export default () => {
   return (
     <div className='front'>
       这是一行字哈哈哈哈
+      <img src={imgUrl} alt="" />
       <Main />
       <About />
     </div>
--- webpack.common.js
+++ webpack.common.js
@@ -6,9 +6,18 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
   entry: './index.tsx',
   output: {
-    filename: '[name].[contenthash].bundle.js',
+    filename: './js/[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    chunkFilename: './js/[id].[contenthash].js',
     clean: true,
+    assetModuleFilename: (pathData) => {
+      // assets资源规则待补充
+      const isImg = pathData.filename.indexOf('.png')
+      if (isImg) {
+        return 'img/[name][ext][query]'
+      }
+      return '[name][ext][query]'
+    }
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -38,10 +47,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        // generator: {
+        //   outputPath: 'img',
+        // },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        // generator: {
+        //   outputPath: 'font',
+        // },
       },
     ],
   },
@@ -52,8 +67,8 @@ module.exports = {
       favicon: './src/assets/img/icon.png',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: "[id].[contenthash].css",
+      filename: './styles/[name].[contenthash].css',
+      chunkFilename: './styles/[id].[contenthash].css',
     }),
   ],
   optimization: {
